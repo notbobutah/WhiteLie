27b214dadbe8127f8b5500db0e806a87327bb4c1
notbobutah <rrmackay@gmail.com>
2018-10-07 10:34:55 -0900

    Fine tuning the autorun

--- scripts/start-picar-lf-oa.sh
+++ scripts/start-picar-lf-oa.sh
@@ -1,2 +1,14 @@
+#!/bin/bash
 
-sudo python /home/pi/Whiteline/example/line_with_obsavoidance.py &
+# Redirect standard out and standard error to a file.
+sudo exec &> /var/log/picar-service.log
+sudo echo $(date +"%D %T")" Begin."
+
+(
+    sleep 30
+    sudo echo $(date +"%D %T")" 30s elapsed, starting picar."
+    sudo python /home/pi/Whiteline/example/line_with_obsavoidance.py &
+) &
+
+exit 0
+
--- scripts/start-picar-lf.sh
+++ scripts/start-picar-lf.sh
@@ -1,2 +1,13 @@
+#!/bin/bash
 
-sudo python /home/pi/Whiteline/example/line_follower.py &
+# Redirect standard out and standard error to a file.
+exec &> /var/log/picar-service.log
+echo $(date +"%D %T")" Begin."
+
+(
+    sleep 30
+    echo $(date +"%D %T")" 30s elapsed, starting picar."
+    sudo python /home/pi/Whiteline/example/line_follower.py &
+) &
+
+exit 0
